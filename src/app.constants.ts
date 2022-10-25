63eae68a5986bc651d3637d78eab6345b7a2c552
Daryl Wong <youcanfinddaryl@gmail.com>
2022-10-25 20:18:39 +0800

    initial commit

---
+++ src/app.constants.ts
@@ -0,0 +1,8 @@
+export const ENDPOINTS = {
+    SIGNUP: "/auth/signup",
+    LOGIN: "/auth/login",
+    GENERATE_2FA: "/auth/generate-2fa-token/",
+    VALIDATE_2FA: "/auth/validate-2fa-token",
+    SSO_LOGIN: "/auth/sso/login",
+    SSO_CALLBACK: "/auth/sso/callback"
+};
