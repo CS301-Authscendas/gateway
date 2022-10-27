67f8719b4d31dd399a24f3981bd61ab064ea13d5
Felice <felicepng2010@gmail.com>
2022-10-28 00:41:30 +0800

    fix: edit endpoints

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,49 +9,49 @@ const ORGANIZATION_PREFIX = "organization";
 
 @Injectable()
 export class UserService {
-    private BASE_URL;
-    constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {
-        this.BASE_URL = configService.get("BASE_USER_URL");
-    }
+  private BASE_URL;
+  constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {
+    this.BASE_URL = configService.get("BASE_USER_URL") + '/user';
+  }
 
-    async getDetailsFromEmail(email: string) {
-        try {
-            const resp = await this.httpService.axiosRef.get(`${this.BASE_URL}/${USER_PREFIX}/${email}`);
-            return resp?.data;
-        } catch (error) {
-            errorHandler(error);
-        }
+  async getDetailsFromEmail(email: string) {
+    try {
+      const resp = await this.httpService.axiosRef.get(`${this.BASE_URL}/${USER_PREFIX}/${email}`);
+      return resp?.data;
+    } catch (error) {
+      errorHandler(error);
     }
+  }
 
-    async fetchUsersByOrg(organizationId: string) {
-        try {
-            const resp = await this.httpService.axiosRef.get(
-                `${this.BASE_URL}/${USER_PREFIX}/${USER_ENDPOINTS.GET_USERS_FROM_ORG}/${organizationId}`,
-            );
-            return resp?.data;
-        } catch (error) {
-            errorHandler(error);
-        }
+  async fetchUsersByOrg(organizationId: string) {
+    try {
+      const resp = await this.httpService.axiosRef.get(
+        `${this.BASE_URL}/${USER_PREFIX}/${USER_ENDPOINTS.GET_USERS_FROM_ORG}/${organizationId}`,
+      );
+      return resp?.data;
+    } catch (error) {
+      errorHandler(error);
     }
+  }
 
-    async updateUserDetails(requestBody: object) {
-        try {
-            const resp = await this.httpService.axiosRef.put(`${this.BASE_URL}/${USER_PREFIX}`, requestBody);
-            return resp?.data;
-        } catch (error) {
-            errorHandler(error);
-        }
+  async updateUserDetails(requestBody: object) {
+    try {
+      const resp = await this.httpService.axiosRef.put(`${this.BASE_URL}/${USER_PREFIX}`, requestBody);
+      return resp?.data;
+    } catch (error) {
+      errorHandler(error);
     }
+  }
 
-    async fetchOrganizationsByList(requestBody: object) {
-        try {
-            const resp = await this.httpService.axiosRef.post(
-                `${this.BASE_URL}/${ORGANIZATION_PREFIX}/${USER_ENDPOINTS.FETCH_ORGANIZATIONS}`,
-                requestBody,
-            );
-            return resp?.data;
-        } catch (error) {
-            errorHandler(error);
-        }
+  async fetchOrganizationsByList(requestBody: object) {
+    try {
+      const resp = await this.httpService.axiosRef.post(
+        `${this.BASE_URL}/${ORGANIZATION_PREFIX}/${USER_ENDPOINTS.FETCH_ORGANIZATIONS}`,
+        requestBody,
+      );
+      return resp?.data;
+    } catch (error) {
+      errorHandler(error);
     }
+  }
 }
