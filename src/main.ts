14411b078d6555b1496700f1e8315543fff55340
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-30 11:30:03 +0800

    feat: add prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
     app.enableCors();
+    app.setGlobalPrefix("/api");
     await app.listen(configService.get("PORT") ?? 3000);
 }
 bootstrap();
